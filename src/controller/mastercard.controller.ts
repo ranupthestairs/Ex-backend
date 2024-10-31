468d08335fb4729e13562838662adbe09c0bd2ba
Bikas <sheriff0613@gmail.com>
2024-10-31 11:17:44 -0500

    added signing key as a env variable and updated related codebase

--- src/controller/mastercard.controller.ts
+++ src/controller/mastercard.controller.ts
@@ -1,8 +1,7 @@
-import { NextFunction, Request, Response } from "express";
-import Axios from "axios";
-import { CONSUMER_KEY, PARTNER_ID } from '../constants';
-import { generateAuthHeader } from "src/utils";
-import fs from 'fs';
+import Axios from 'axios';
+import { NextFunction, Request, Response } from 'express';
+import { generateAuthHeader } from '../utils';
+import { CONSUMER_KEY, PARTNER_ID, SIGNING_KEY } from '../constants';
 
 export const createTransaction = async (
     req: Request,
@@ -11,14 +10,20 @@ export const createTransaction = async (
 ) => {
     const data = req.body;
     const url = `https://sandbox.api.mastercard.com/send/static/v1/partners/${PARTNER_ID}/transfers/payment`;
-    const signingKey = fs.readFileSync('../../myrsa.key');
-    const authHeader = generateAuthHeader(url, 'POST', JSON.stringify(data), CONSUMER_KEY, signingKey);
+    const authHeader = generateAuthHeader(
+        url,
+        'POST',
+        JSON.stringify(data),
+        CONSUMER_KEY,
+        SIGNING_KEY,
+    );
     try {
         Axios.post(url, data, {
             headers: {
-                'Authorization': authHeader,
+                Authorization: authHeader,
             },
-        }).then((response) => {
+        })
+            .then((response) => {
                 // validate the data
                 res.status(200).send(response.data);
             })
@@ -28,11 +33,11 @@ export const createTransaction = async (
                     message: 'Error',
                     error: error.message,
                 });
-            })
-        } catch (err) {
-            return next(err);
-        }
+            });
+    } catch (err) {
+        return next(err);
     }
+};
 
 export const getTransactionStatusById = async (
     req: Request,
@@ -41,22 +46,25 @@ export const getTransactionStatusById = async (
 ) => {
     const { id } = req.params;
     const url = `https://sandbox.api.mastercard.com/send/static/v1/partners/${PARTNER_ID}/transfers/${id}`;
-    const signingKey = fs.readFileSync('../../myrsa.key');
-    const authHeader = generateAuthHeader(url, 'GET', '', CONSUMER_KEY, signingKey);
+    const authHeader = generateAuthHeader(
+        url,
+        'GET',
+        '',
+        CONSUMER_KEY,
+        SIGNING_KEY,
+    );
 
     if (!id) return res.status(400).send({ message: 'Invalid parameter' });
     try {
         Axios.get(url, {
             headers: {
-                'Authorization': authHeader,
-            }
+                Authorization: authHeader,
+            },
         })
             .then((response) => {
-                console.log('Response:', response.data);
                 res.status(200).send(response.data);
             })
             .catch((error) => {
-                console.log('getTransactionStatusById Error:', error.message);
                 res.status(500).send({
                     message: 'Error',
                     error: error.message,
@@ -65,4 +73,4 @@ export const getTransactionStatusById = async (
     } catch (err) {
         return next(err);
     }
-}
+};
--- src/routes/mastercard.routes.ts
+++ src/routes/mastercard.routes.ts
@@ -3,6 +3,7 @@ import * as mastercardController from '../controller/mastercard.controller';
 
 const routes = Router();
 
+routes.post('/payment', mastercardController.createTransaction);
 routes.get('/status/:id', mastercardController.getTransactionStatusById);
 
 export default routes;
