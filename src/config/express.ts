08200c6e44354cc30c26a096ecf5118653af68d5
Bikas <bikaslin600@gmail.com>
2024-11-08 05:58:54 -0600

    added cors config

--- src/config/express.ts
+++ src/config/express.ts
@@ -4,23 +4,11 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import { routes } from '../routes';
 
-const whiteList = ['http://localhost:3000', 'https://app.tahreef.com'];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whiteList.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-};
-
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use('/', routes);
